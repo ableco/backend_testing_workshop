cd406d2c26b3d5b2b42dd050e20d66e1f73f37bd
juanjose <juanjose@able.co>
2021-04-22 15:12:21 -0500

    Tests

--- spec/controllers/collections_controller_spec.rb
+++ spec/controllers/collections_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe CollectionsController, type: :controller do
+    context "follow request" do
+    let(:current_user){create(:user)}
+    let(:another_user){create(:user)}
+    let(:collection){create(:collection)}
+    let(:collection2){create(:collection)}
+    it "401 status when expired date" do
+      videogame = create(:videogame, collection_id: collection.id)
+      subscription = create(:subscription, collection_id: collection.id, user_id: current_user.id, expiration_date: DateTime.current.to_date)
+      get(:show, params: { id: collection.id, user_id: current_user.id })
+      expect(response.status).to eq(401)
+    end
 
+    it "401 status" do
+      videogame = create(:videogame, collection_id: collection.id)
+      subscription = create(:subscription, collection_id: collection.id, user_id: current_user.id, expiration_date: DateTime.current.to_date + 1.month)
+      get(:show, params: { id: collection2.id , user_id: current_user.id })
+      expect(response.status).to eq(401)
+    end
+  end
 end
--- spec/controllers/videogames_controller_spec.rb
+++ spec/controllers/videogames_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe VideogamesController type: :controller do
+RSpec.describe VideogamesController, type: :controller do
+  context "follow request" do
+    let(:current_user){create(:user)}
+    let(:another_user){create(:user)}
+    let(:collection){create(:collection)}
+    let(:collection2){create(:collection)}
+    it "200 status" do
+      videogame = create(:videogame, collection_id: collection.id)
+      subscription = create(:subscription, collection_id: collection.id, user_id: current_user.id, expiration_date: DateTime.current.to_date + 1.month)
+      raw_response = get(:show, params: { id: videogame.id ,user_id: current_user.id, collection_id: collection.id })
+      body = JSON.parse(raw_response.body)
+      expect(body[0]["id"]).to eq(videogame.id)
+      expect(response.status).to eq(200)
+    end
 
+    it "401 status when try to access to a videogame that is not subscribed" do
+      videogame = create(:videogame, collection_id: collection.id)
+      subscription = create(:subscription, collection_id: collection.id, user_id: current_user.id, expiration_date: DateTime.current.to_date + 1.month)
+      get(:show, params: { id: videogame.id, user_id: current_user.id, id: videogame.id , collection_id: collection2.id,})
+      expect(response.status).to eq(401)
+    end
+  end
 end
