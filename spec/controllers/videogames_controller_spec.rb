97a10f46ea5213c58423858e51902f2bbdf81ea8
juanjose <juanjose@able.co>
2021-04-22 16:28:11 -0500

    Update videogame test

--- spec/controllers/videogames_controller_spec.rb
+++ spec/controllers/videogames_controller_spec.rb
@@ -17,8 +17,9 @@ RSpec.describe VideogamesController, type: :controller do
 
     it "Validate the 401 status when the user tries to access using the “GET Method” to the “Show View” for a videogame that is not in their collection." do
       videogame = create(:videogame, collection_id: collection.id)
+      videogame2 = create(:videogame, collection_id: collection2.id)
       subscription = create(:subscription, collection_id: collection.id, user_id: current_user.id, expiration_date: DateTime.current.to_date + 1.month)
-      get(:show, params: { id: videogame.id, user_id: current_user.id, collection_id: collection2.id })
+      get(:show, params: { id: videogame2.id, user_id: current_user.id, collection_id: collection.id })
       expect(response.status).to eq(401)
     end
   end
